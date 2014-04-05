512c835c17650f9d3ccb217753f17593a2bb4279
Justin Love <git@JustinLove.name>
2014-04-04 21:47:30 -0500

    identify candidate selections

--- ui/mods/missile_command/bootstrap.js
+++ ui/mods/missile_command/bootstrap.js
@@ -4,4 +4,9 @@ require.config({
     text: 'missile_command/text',
   }
 })
+// make the object keys exist for Panel.ready
+var missile_command_stub = function() {}
+_.defaults(handlers, {
+  selection: missile_command_stub
+})
 require(['missile_command/main'])
--- ui/mods/missile_command/missile_command.js
+++ ui/mods/missile_command/missile_command.js
@@ -3,11 +3,24 @@ define([
 ], function(html) {
   "use strict";
 
+  var nuke_launcher = '/pa/units/land/nuke_launcher/nuke_launcher.json'
+
+  var originalSelection = handlers.selection
+  handlers.selection = function(payload) {
+    var si = payload.spec_ids
+    if (Object.keys(si).length == 1) {
+      if (si[nuke_launcher] && si[nuke_launcher].length == 1) {
+        console.log(si[nuke_launcher])
+      }
+    }
+    originalSelection(payload)
+  }
+
   return {
     ready: function() {
       var container = $(html)
       container.appendTo('body')
-      ko.applyBindings(model, container)
+      ko.applyBindings(model, container[0])
     }
   }
 })
