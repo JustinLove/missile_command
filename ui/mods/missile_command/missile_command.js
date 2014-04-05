ba1c29d4271493fcd3145eec7b6b7cddde8db596
Justin Love <git@JustinLove.name>
2014-04-04 21:58:42 -0500

    capture group

--- ui/mods/missile_command/missile_command.js
+++ ui/mods/missile_command/missile_command.js
@@ -5,12 +5,21 @@ define([
 
   var nuke_launcher = '/pa/units/land/nuke_launcher/nuke_launcher.json'
 
+  var registry = []
+
   var originalSelection = handlers.selection
   handlers.selection = function(payload) {
     var si = payload.spec_ids
     if (Object.keys(si).length == 1) {
       if (si[nuke_launcher] && si[nuke_launcher].length == 1) {
-        console.log(si[nuke_launcher])
+        var id = si[nuke_launcher][0]
+        console.log(id)
+        if (!registry.some(function(r) {return r.id == id})) {
+          console.log('unknown')
+          api.select.captureGroup(id)
+          registry.push({id: id})
+          console.log(registry)
+        }
       }
     }
     originalSelection(payload)
