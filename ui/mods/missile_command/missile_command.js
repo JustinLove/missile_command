2119feac3d4d4f1d30c63c8599da50191d59a7de
Justin Love <git@JustinLove.name>
2014-04-04 22:31:33 -0500

    visual list, click to select

--- ui/mods/missile_command/missile_command.js
+++ ui/mods/missile_command/missile_command.js
@@ -5,7 +5,19 @@ define([
 
   var nuke_launcher = '/pa/units/land/nuke_launcher/nuke_launcher.json'
 
-  var registry = []
+  var registry = ko.observableArray()
+
+  var missileViewModel = {
+    clone: function(id) {
+      var missile = Object.create(this)
+      missile.id = id
+      return missile
+    },
+    select: function() {
+      console.log(this.id)
+      api.select.recallGroup(this.id)
+    }
+  }
 
   var originalSelection = handlers.selection
   handlers.selection = function(payload) {
@@ -13,23 +25,25 @@ define([
     if (Object.keys(si).length == 1) {
       if (si[nuke_launcher] && si[nuke_launcher].length == 1) {
         var id = si[nuke_launcher][0]
-        console.log(id)
-        if (!registry.some(function(r) {return r.id == id})) {
-          console.log('unknown')
+        if (!_.find(registry(), function(m) {return m.id == id})) {
+          console.log('unknown', id)
           api.select.captureGroup(id)
-          registry.push({id: id})
-          console.log(registry)
+          registry.push(missileViewModel.clone(id))
         }
       }
     }
     originalSelection(payload)
   }
 
+  var viewModel = {
+    registry: registry
+  }
+
   return {
     ready: function() {
       var container = $(html)
       container.appendTo('body')
-      ko.applyBindings(model, container[0])
+      ko.applyBindings(viewModel, container[0])
     }
   }
 })
