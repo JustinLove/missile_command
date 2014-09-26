fe2adf00dbbea08c4f93a926ab2aa92b1e110f94
Justin Love <git@JustinLove.name>
2014-09-25 23:08:21 -0500

    visually add an option bar icon

---
+++ ui/mods/missile_command/live_game_options_bar.js
@@ -0,0 +1,10 @@
+(function() {
+  model.toggleMissileCommand = function() {}
+  model.missileCommandImage = ko.observable('img/ingame_options_bar/pip_on.png')
+  $('.div_ingame_options_bar_cont').prepend(
+  '<div class="btn_ingame_options div_toggle_missile_command">' + 
+      '<a href="#" data-bind="click: toggleMissileCommand">' + 
+          '<img data-bind="attr: { src: missileCommandImage }" />' + 
+      '</a>' +
+  '</div>')
+})()
