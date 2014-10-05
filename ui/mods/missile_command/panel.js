c29d8f5e5589e19305c75aad0c2bd21253cb537e
Justin Love <git@JustinLove.name>
2014-10-05 09:31:27 -0500

    move to left

--- ui/mods/missile_command/panel.js
+++ ui/mods/missile_command/panel.js
@@ -5,12 +5,12 @@ define(function() {
       position: 'absolute',
       top: 100,
       bottom: 100,
-      right: 50,
+      left: 0,
     }).attr({
       src: "coui://ui/mods/missile_command/missile_command.html",
       'no-keyboard': true,
       'yield-focus': true,
-      fit: "dock-top-right",
+      fit: "dock-top-left",
       'data-bind': 'visible: visible'
     })
     $panel.appendTo('body')
