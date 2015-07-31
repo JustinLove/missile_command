03526af805d0cfcafb05b5773ee6f31487d0aa04
Justin Love <git@JustinLove.name>
2015-07-30 21:31:28 -0500

    extract event hooks

---
+++ ui/mods/missile_command/alerts.js
@@ -0,0 +1,40 @@
+define([
+  'missile_command/panel',
+], function(panel) {
+  "use strict";
+
+  var pendingEvents = []
+  var missileEvents = function(events) {
+    if (events.length < 1) return
+
+    if (api.panels.missile_command) {
+      api.panels.missile_command.message('missile_command_events', events)
+    } else {
+      pendingEvents = pendingEvents.concat(events)
+      panel.show()
+    }
+  }
+
+  var myMissileEvent = function(alert) {
+    return (
+      (alert.watch_type == constants.watch_type.ready ||
+       alert.watch_type == constants.watch_type.death) && 
+      eventSystem.isType(constants.unit_type.Nuke, alert.unit_types)
+    )
+  }
+
+  var live_game_watch_list = handlers.watch_list
+  handlers.watch_list = function(payload) {
+    //console.log(payload);
+
+    if (live_game_watch_list) {
+      live_game_watch_list(payload)
+    }
+
+    missileEvents(payload.list.filter(myMissileEvent))
+  }
+
+  return {
+    pendingEvents: pendingEvents
+  }
+})
--- ui/mods/missile_command/live_game.js
+++ ui/mods/missile_command/live_game.js
@@ -2,9 +2,10 @@ define([
   'missile_command/panel',
   'missile_command/lobby_id',
   'missile_command/specs',
+  'missile_command/alerts',
   'missile_command/attack_tracking',
   'missile_command/preview_window',
-], function(panel, lobbyId, specs) {
+], function(panel, lobbyId, specs, alerts) {
   "use strict";
 
   model.player.subscribe(function(player) {
@@ -12,44 +13,14 @@ define([
     specs.nuke_launcher = specs.base_nuke_launcher + player.spec_tag
   })
 
-  var pendingEvents = []
-  var missileEvents = function(events) {
-    if (events.length < 1) return
-
-    if (api.panels.missile_command) {
-      api.panels.missile_command.message('missile_command_events', events)
-    } else {
-      pendingEvents = pendingEvents.concat(events)
-      panel.show()
-    }
-  }
-
-  var myMissileEvent = function(alert) {
-    return (
-      (alert.watch_type == constants.watch_type.ready ||
-       alert.watch_type == constants.watch_type.death) && 
-      eventSystem.isType(constants.unit_type.Nuke, alert.unit_types)
-    )
-  }
-
-  var live_game_watch_list = handlers.watch_list
-  handlers.watch_list = function(payload) {
-    //console.log(payload);
-    missileEvents(payload.list.filter(myMissileEvent))
-
-    if (live_game_watch_list) {
-      live_game_watch_list(payload)
-    }
-  }
-
   handlers.missile_command_hello = function() {
     console.log('hello', api.panels.missile_command)
     api.panels.missile_command.message('missile_command_state', {
       lobbyId: lobbyId(),
       spec_tag: model.player().spec_tag,
-      pendingEvents: pendingEvents
+      pendingEvents: alerts.pendingEvents
     });
-    pendingEvents = []
+    alerts.pendingEvents = []
 
     api.panels.options_bar && api.panels.options_bar.message('missile_command_loading', false)
   }
